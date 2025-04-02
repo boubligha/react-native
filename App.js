f103f6cb117c029ac4de2de1d90aa2b95386a653
boubligha <151193079+boubligha@users.noreply.github.com>
2025-04-02 13:24:34 +0000

    style depending on operative system

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button, Alert } from 'react-native';
+import { StyleSheet, Text, View,Button, Alert,StatusBar, Platform } from 'react-native';
 
 export default function App() {
   let x = 0;
@@ -8,9 +8,18 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      //button that shows title with confirmation message with 2 buttons
       <Button
         title="Press me"
         onPress={() => Alert.alert('Mytitle','My message',[
+          {text: 'Yes', onPress: () => console.log('Ok Pressed')},
+          {text: 'No', onPress: () => console.log('canceled Pressed')},
+        ])}
+        />
+        //button that shows title with confirmation message and textfield to write somthing with 2 buttons
+        <Button
+        title="Press me"
+        onPress={() => Alert.prompt('Mytitle','My message',text=>console.log(text),[
           {text: 'Yes', onPress: () => console.log('Ok Pressed')},
           {text: 'No', onPress: () => console.log('canceled Pressed')},
         ])}
@@ -25,5 +34,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 10 : 0,
+    //paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
