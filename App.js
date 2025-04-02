31e86ba88dfc8d897bd6f9d232138db4c5419d18
boubligha <151193079+boubligha@users.noreply.github.com>
2025-04-02 12:37:50 +0000

    button + alertDialogue

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button, Alert } from 'react-native';
 
 export default function App() {
+  let x = 0;
+  console.log("app executed");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Mytitle','My message',[
+          {text: 'Yes', onPress: () => console.log('Ok Pressed')},
+          {text: 'No', onPress: () => console.log('canceled Pressed')},
+        ])}
+        />
     </View>
   );
 }
